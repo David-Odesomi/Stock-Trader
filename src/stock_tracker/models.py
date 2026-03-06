fc91b4a190e8193b111762745e689860c6d28f9c
Odesomi David <davidodesomi@Gmail.com>
2026-03-06 20:39:47 +0100

    feat: add Stock and Portfolio models

---
+++ src/stock_tracker/models.py
@@ -0,0 +1,15 @@
+class Stock:
+    def __init__(self, symbol, buy_price, shares) -> None:
+        self.symbol = symbol
+        self.buy_price = buy_price
+        self.shares = shares
+
+    def current_value(self, current_price):
+        return current_price * self.shares
+
+class Portfolio:
+    def __init__(self):
+        self.stocks = []
+
+    def add_stock(self, stock):
+        self.stocks.append(stock)
