7109fc75dd5c52dec06a925d4238f3727840d2a6
Odesomi David <davidodesomi@Gmail.com>
2026-03-07 09:14:03 +0100

    feat: add portfolio save and load functions

---
+++ src/stock_tracker/storage.py
@@ -0,0 +1,18 @@
+import json
+from src.stock_tracker.models import Stock, Portfolio
+
+def save_portfolio(portfolio, filepath):
+    data = [{"symbol": stock.symbol, "buy_price": stock.buy_price, "shares": stock.shares} for stock in portfolio.stocks]
+
+    with open(filepath,"w") as f:
+        json.dump(data, f)
+
+def load_portfolio(filepath):
+    with open(filepath, "r") as f:
+        data= json.load(f)
+
+    portfolio = stock
+    for item in data:
+        stock = Stock(item["symbol"], item["buy_price"], item["shares"])
+        portfolio.add_stock(stock)
+    return portfolio
